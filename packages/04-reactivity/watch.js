f1a1a8a32e200168dc23ee7d5bbc41f0d8ca15d1
zhangtao.kylee <zhangtao.kylee@bytedance.com>
2024-07-04 10:52:51 +0800

    :beer: 第4章终于收工

---
+++ packages/04-reactivity/watch.js
@@ -0,0 +1,57 @@
+import { effect } from './effect'
+
+function traverse(value, seen = new Set()) {
+    if (typeof value !== 'object' || value === null || seen.has(value)) return
+    seen.add(value)
+    for (const k in value) {
+        traverse(value[k], seen)
+    }
+
+    return value
+}
+
+export const watch = (source, cb, options = {}) => {
+    let getter
+    if (typeof source === 'function') {
+        getter = source
+    } else {
+        getter = () => traverse(source)
+    }
+
+    let oldValue, newValue
+
+    // 存储用户注册的过期回调函数
+    let cleanup
+    function onInvalidate(fn) {
+        cleanup = fn
+    }
+
+    const job = () => {
+        newValue = effectFn()
+        // 调用回调函数之前先调用过期回调
+        if (cleanup) {
+            cleanup()
+        }
+        cb(newValue, oldValue, onInvalidate)
+        oldValue = newValue
+    }
+
+    const effectFn = effect(() => getter(), {
+        lazy: true,
+        scheduler: () => {
+            // 放到微任务队列中在组件更新后执行
+            if (options.flush === 'post') {
+                const p = Promise.resolve()
+                p.then(job)
+            } else {
+                job()
+            }
+        }
+    })
+
+    if (options.immediate) {
+        job()
+    } else {
+        oldValue = effectFn()
+    }
+}
